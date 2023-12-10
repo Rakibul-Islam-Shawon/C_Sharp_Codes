8c42c8e910e40808d510a801ad0160af0fe7e755
Md Rakibul Islam <shawonnstu@gmail.com>
2023-12-10 16:04:14 +0100

    Multiplication of Threee number

---
+++ HelloWorld/Multiplication of Two Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_of_Two_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 12;
+            int number2 = 14;
+            int number3 = 17;
+
+            int mul_result = number1 * number2 * number3;
+
+            Console.WriteLine("The result is: " + mul_result);
+            Console.ReadLine();
+        }
+    }
+}
