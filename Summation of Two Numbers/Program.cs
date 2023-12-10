ab41a5d5405722205d187ed567931c70d0d99015
Md Rakibul Islam <shawonnstu@gmail.com>
2023-12-10 01:14:15 +0100

    Summation of two integer number for practice

---
+++ Summation of Two Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summation_of_Two_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int FirstNumber = 100;
+            int SecondNumber = 230;
+
+            int sum = FirstNumber + SecondNumber;
+
+            Console.WriteLine("The sum of " + FirstNumber + " and " + SecondNumber + " is : " + sum);
+            Console.ReadLine();
+        
+        }
+    }
+}
