9a08c2a9657c2755c4806020c77ac9a16a458156
Md Rakibul Islam <shawonnstu@gmail.com>
2023-12-10 01:09:13 +0100

    Added gitignore file

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, This is my first C# Code");
+            Console.WriteLine("I will try to push everything in Github");
+            Console.ReadLine();
+        }
+    }
+}
