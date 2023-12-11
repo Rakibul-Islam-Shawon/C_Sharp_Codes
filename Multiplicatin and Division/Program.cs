69523c74d5e2d6131fbacb72b2d9c91e6673ca79
Md Rakibul Islam <shawonnstu@gmail.com>
2023-12-11 23:29:02 +0100

    Multiplication and Division C# program added

---
+++ Multiplicatin and Division/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplicatin_and_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 100;
+            int num2 = 50;
+
+            int mul_result = num1 * num2;
+            int div_result = num1 / num2;
+
+            Console.WriteLine(mul_result);
+            Console.WriteLine(div_result);
+
+            Console.ReadLine();
+        }
+    }
+}
