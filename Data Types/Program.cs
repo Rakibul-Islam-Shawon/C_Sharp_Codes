43da238011642b72d5c80760aad09a59c7192083
Md Rakibul Islam <shawonnstu@gmail.com>
2023-12-14 01:21:57 +0100

    Different types of Data types added

---
+++ Data Types/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Integer*/
+
+            int a = 100;
+            Console.WriteLine(a);
+
+            //Floating point
+            double f = 10.34355;
+            Console.WriteLine(f);   
+
+            //Character
+            char c = 'a';
+            Console.WriteLine(c);
+
+            //String
+
+            string st = "Hello";
+
+            Console.WriteLine(st);
+
+            Console.ReadLine();
+
+        }
+    }
+}
