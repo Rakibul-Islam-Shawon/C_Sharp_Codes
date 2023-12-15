38813d75adfaf9265173c860d52f57285ddeb570
Md Rakibul Islam <shawonnstu@gmail.com>
2023-12-15 23:49:09 +0100

    New Floating number Used

---
+++ Float Data Types/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Float_Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float Num = 10.3F;
+
+            Console.WriteLine(Num);
+            Console.ReadLine();
+
+
+        }
+    }
+}
