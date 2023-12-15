69ed04e09439bae25c6caba28147011598786161
Md Rakibul Islam <shawonnstu@gmail.com>
2023-12-16 00:06:21 +0100

    Double Data Type Used

---
+++ Double Data Type/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Double_Data_Type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = 12.59601;
+            Console.WriteLine(number);
+            Console.ReadLine();
+        }
+    }
+}
