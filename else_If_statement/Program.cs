2f24d2f01cd1e2bd6970c0cb4f4a2fa0e8570c23
Md Rakibul Islam <shawonnstu@gmail.com>
2023-12-19 07:29:38 +0100

    If..else if statement used

---
+++ else_If_statement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace else_If_statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1 , 2 or 3: ");
+            string userValue = Console.ReadLine();
+
+            if (userValue == "1")
+            {
+                string message = "You won a new car. ";
+                Console.WriteLine(message);
+            }
+
+            else if(userValue == "2")
+            {
+                string message = "You won a boat !";
+                Console.WriteLine(message);
+            }
+
+            else if(userValue == "3")
+            {
+                string message = "You won a cat!";
+                Console.WriteLine(message);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
