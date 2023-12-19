6167d4beeecae796f1ccb2e04315d60ae67fbb71
Md Rakibul Islam <shawonnstu@gmail.com>
2023-12-19 07:36:26 +0100

    Decision making statement with some modifications

--- else_If_statement/Program.cs
+++ else_If_statement/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,32 +14,32 @@ namespace else_If_statement
             Console.WriteLine("Bob's Big Giveaway");
             Console.Write("Choose a door: 1 , 2 or 3: ");
             string userValue = Console.ReadLine();
+            string message = "";
 
             if (userValue == "1")
             {
-                string message = "You won a new car. ";
-                Console.WriteLine(message);
+                 message = "You won a new car. ";
+               
             }
 
             else if(userValue == "2")
             {
-                string message = "You won a boat !";
-                Console.WriteLine(message);
+                 message = "You won a boat !";
+               
             }
 
             else if(userValue == "3")
             {
-                string message = "You won a cat!";
-                Console.WriteLine(message);
+                 message = "You won a cat!";
             }
 
             else
             {
-                string message = "Sorry, we don't understand";
-                Console.WriteLine(message);
+                 message = "Sorry, we didn't understand";
+                
             }
 
-
+            Console.WriteLine(message);
             Console.ReadLine();
         }
     }
