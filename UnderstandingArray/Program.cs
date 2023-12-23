893a4ea4ff7fe6cceca6d444b96b3a00bee08129
Md Rakibul Islam <shawonnstu@gmail.com>
2023-12-23 23:49:05 +0100

    Array with string

--- UnderstandingArray/Program.cs
+++ UnderstandingArray/Program.cs
@@ -10,16 +10,26 @@ namespace UnderstandingArray
     {
         static void Main(string[] args)
         {
-            int[] numbers = new int[5];
+            /* int[] numbers = new int[5];
 
-            numbers[0] = 2;
-            numbers[1] = 4;
-            numbers[2] = 6;
-            numbers[3] = 8;
-            numbers[4] = 10;
+             numbers[0] = 2;
+             numbers[1] = 4;
+             numbers[2] = 6;
+             numbers[3] = 8;
+             numbers[4] = 10;
 
-            Console.WriteLine(numbers.Length);
-            Console.WriteLine(numbers[2]);
+             Console.WriteLine(numbers.Length);
+             Console.WriteLine(numbers[2]);
+             Console.ReadLine();*/
+
+
+            string[] names = new string[] { "Rakib", "Shawon", "Rifat", "Sakib", "Arman" };
+
+            for (int i = 0; i < names.Length; i++) 
+            {
+                Console.WriteLine(names[i]);
+            }
+
             Console.ReadLine();
         }
     }
