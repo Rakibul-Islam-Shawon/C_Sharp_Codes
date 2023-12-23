fa99aac93cf72bff5be9d53ba3abdb08f0c54527
Md Rakibul Islam <shawonnstu@gmail.com>
2023-12-23 23:14:37 +0100

    For Iterations

--- ForLoop/Program.cs
+++ ForLoop/Program.cs
@@ -12,7 +12,16 @@ namespace ForLoop
         {
             for(int i = 0; i < 10; i++)
             {
-                Console.WriteLine(i);
+                if (i == 7)
+                {
+                    Console.WriteLine("Found seven.");
+                }
+            }
+
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);
+                
             }
             Console.ReadLine();
         }
