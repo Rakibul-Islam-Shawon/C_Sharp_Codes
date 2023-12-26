3132fee692c8740c2bfa95e1f3ad88c085add6b1
Md Rakibul Islam <shawonnstu@gmail.com>
2023-12-26 15:14:07 +0100

    Working with String

---
+++ WorkingWithString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "My \"So-Called\"life";
+
+            Console.WriteLine(myString);
+            Console.ReadLine();
+        }
+    }
+}
