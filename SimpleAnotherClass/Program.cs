5fffba43d21f496ad0d464f6df63082f7e76b90b
Md Rakibul Islam <shawonnstu@gmail.com>
2024-01-03 23:55:06 +0100

    Simple add with class

---
+++ SimpleAnotherClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAnotherClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition a = new Addition();
+
+            Console.WriteLine(a.add());
+        }
+    }
+
+    public class Addition
+    {
+        public int a = 10;
+        public int b = 20;  
+
+        public int add()
+        {
+            return a + b;
+        }
+        
+    }
+}
