daa7857f47a5993d5873e5774b843091607ca2d5
Md Rakibul Islam <shawonnstu@gmail.com>
2024-01-06 00:00:23 +0100

    ElseIf program

---
+++ IfElseProgram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            if (a > b)
+            {
+                Console.WriteLine("A greater than B");
+
+            }
+            else
+            {
+                Console.WriteLine("B greater than A");
+            }
+
+            
+
+        }
+    }
+}
