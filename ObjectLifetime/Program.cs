7cc8a44c3445f8c756c3510344fdc3b2367a652c
Md Rakibul Islam <shawonnstu@gmail.com>
2024-01-06 11:18:23 +0100

    New file called ObjectLifetime created for understanding Class

---
+++ ObjectLifetime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLifetime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+     class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get;set; }
+
+
+    }
+}
