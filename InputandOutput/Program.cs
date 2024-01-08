9c8b358341d3e716e226e773d269326bd6e0ffe3
Md Rakibul Islam <shawonnstu@gmail.com>
2024-01-08 23:13:41 +0100

    Simple input output code

---
+++ InputandOutput/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputandOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your first name: ");
+            string fName= Console.ReadLine();
+            Console.WriteLine("What is your Last Name: ");
+            string lName = Console.ReadLine();
+
+            Console.WriteLine("Congratiolations ! {0} {1} , Your are a programmer1", fName, lName);
+            Console.ReadLine();
+        }
+    }
+}
