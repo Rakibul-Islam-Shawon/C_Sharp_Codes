99fb2e668ef0e96ebae63b68c997a73d3f192a2d
Md Rakibul Islam <shawonnstu@gmail.com>
2024-01-08 23:26:54 +0100

    Simple Loop program for practice

---
+++ SimpleLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+
+            for(int i = 0; i< a; i++)
+            {
+                Console.WriteLine(i);
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
