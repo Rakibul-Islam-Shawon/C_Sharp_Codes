f36fbbfc8c9e4cdddb68cf21807af462be032269
Md Rakibul Islam <shawonnstu@gmail.com>
2024-01-13 23:18:46 +0100

    String input and output practice

---
+++ StringINputOutput/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringINputOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Your name please: ");
+            string Name = Console.ReadLine();
+
+            Console.Write("Your address please: ");
+            string Address = Console.ReadLine();
+
+
+            Console.WriteLine("Your name is: {0} and your Address is: {1}.", Name, Address);
+            Console.ReadLine();
+        }
+    }
+}
