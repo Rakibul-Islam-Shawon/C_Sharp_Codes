31e90ca539dce5cc0b9b90c3ed860de4df48952e
Md Rakibul Islam <shawonnstu@gmail.com>
2024-01-17 23:28:24 +0100

    REad a and b form keyboard

--- IfElseProgram/Program.cs
+++ IfElseProgram/Program.cs
@@ -10,13 +10,21 @@ namespace IfElseProgram
     {
         static void Main(string[] args)
         {
-            int a = 10;
-            int b = 20;
+            
+            Console.Write("Enter the value of a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the value of b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
 
             if (a > b)
             {
                 Console.WriteLine("A greater than B");
 
+            }
+            else if (a == b)
+            {
+                Console.WriteLine("A is similar to B");
             }
             else if( a < b)
             {
