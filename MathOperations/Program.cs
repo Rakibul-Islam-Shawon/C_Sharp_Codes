62dcfdaaa6c362db9bd612c3ac6b3c0603561e4a
Md Rakibul Islam <shawonnstu@gmail.com>
2024-01-19 23:27:47 +0100

    Math operations added

---
+++ MathOperations/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Math.Max(34, 345);
+
+            Console.WriteLine(result);
+        }
+    }
+}
