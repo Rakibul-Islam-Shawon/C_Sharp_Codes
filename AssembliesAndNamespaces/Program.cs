47e1054b72a6fbce781ca7154ed7e98c6691c5c0
Md Rakibul Islam <shawonnstu@gmail.com>
2024-01-20 23:37:52 +0100

    Another built in class used here

--- AssembliesAndNamespaces/Program.cs
+++ AssembliesAndNamespaces/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Net;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace AssembliesAndNamespaces
 {
@@ -7,11 +9,15 @@ namespace AssembliesAndNamespaces
     {
         static void Main(string[] args)
         {
+                //string text = "A class is the most powerful data type in C#. ";
 
-            string text = "A class is the most powerful data type in C#. ";
+            //File.WriteAllText(@"E:\2019-Programming Lang\C#\WriteText.txt", text);
 
-            File.WriteAllText(@"E:\2019-Programming Lang\C#\WriteText.txt", text);
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
 
+            Console.WriteLine(reply);
+            File.WriteAllText(@"E:\2019-Programming Lang\C#\WriteText.txt", reply);
             Console.WriteLine("Hello world!");
             Console.ReadLine();
         }
