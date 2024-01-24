8e3ed9c93e8be1a3e94017a6e634957b29189a07
Md Rakibul Islam <shawonnstu@gmail.com>
2024-01-24 23:25:31 +0100

    New string program added

---
+++ StringLearning/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "Hello World";
+            Console.WriteLine(txt.ToUpper());   // Outputs "HELLO WORLD"
+            Console.WriteLine(txt.ToLower());   // Outputs "hello world"
+        }
+    }
+}
