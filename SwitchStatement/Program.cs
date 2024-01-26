a13d609bdf14e1486307f4c0c3c9e0d481d9a5cd
Md Rakibul Islam <shawonnstu@gmail.com>
2024-01-26 23:46:18 +0100

    Switch statement elaboration

---
+++ SwitchStatement/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchStatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age = 78;
+
+            switch (age)
+            {
+                case 18:
+                    Console.WriteLine("Please wait for an Year");
+                    break;
+
+                case 20:
+                    Console.WriteLine("You are 20");
+                    break;
+
+                default:
+                    Console.WriteLine("Enjoy");
+                    break;
+            }
+        }
+    }
+}
