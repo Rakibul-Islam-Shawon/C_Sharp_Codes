38aa4ce0a79874663e434d96543e7e15c8a193f1
Md Rakibul Islam <shawonnstu@gmail.com>
2024-01-30 23:44:59 +0100

    Method program introduction

--- LoopsStatment/Program.cs
+++ LoopsStatment/Program.cs
@@ -36,6 +36,8 @@ namespace LoopsStatment
             }
 
 
+
+
             Console.ReadLine();
         }
     }
---
+++ Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Greet()
+        {
+            Console.WriteLine("Good Morning.");
+        }
+        static void Main(string[] args)
+        {
+            Greet();
+        }
+    }
+}
