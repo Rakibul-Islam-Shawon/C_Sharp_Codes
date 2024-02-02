fde4715d3184e8876b61e9979ae1c3d20824d24d
Md Rakibul Islam <shawonnstu@gmail.com>
2024-02-02 23:09:18 +0100

    Here Given an example of Boolean data Type

---
+++ Boolean/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool _Name = true;
+
+            if (_Name)
+            {
+                Console.WriteLine("The value is true.");
+            }
+            else
+            {
+                Console.WriteLine("The value is false.");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
